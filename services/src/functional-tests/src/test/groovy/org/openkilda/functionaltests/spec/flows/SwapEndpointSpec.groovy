2b1665997281848fbb2d0a723cb4a4741b98d192
Yaroslav Lobankov <lobankovyv@gmail.com>
2019-05-22 21:26:54 +0300

    Extend test with DDT approach

--- services/src/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/flows/SwapEndpointSpec.groovy
+++ services/src/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/flows/SwapEndpointSpec.groovy
@@ -1,60 +1,57 @@
 package org.openkilda.functionaltests.spec.flows
 
 import org.openkilda.functionaltests.BaseSpecification
+import org.openkilda.functionaltests.helpers.PathHelper
 import org.openkilda.messaging.payload.flow.SwapFlowPayload
+
+import spock.lang.Unroll
 
 class SwapEndpointSpec extends BaseSpecification {
 
+    @Unroll
     def "Able to swap endpoints for flows with the same source and destination switches"() {
         given: "Two flows with the same source and destination switches"
-        def switchPair = topologyHelper.getNotNeighboringSwitchPair()
-        def flow1 = flowHelper.randomFlow(switchPair)
-        def flow2 = flowHelper.randomFlow(switchPair, false, [flow1])
-
         flowHelper.addFlow(flow1)
         flowHelper.addFlow(flow2)
 
-        when: "Try to swap source endpoints for flows"
+        when: "Try to swap endpoints for flows"
+        def flow1Path = PathHelper.convert(northbound.getFlowPath(flow1.id))
+        def flow2Path = PathHelper.convert(northbound.getFlowPath(flow2.id))
+
         def response = northbound.swapFlowEndpoint(
-                new SwapFlowPayload(flow1.id, flow2.source, flow1.destination),
-                new SwapFlowPayload(flow2.id, flow1.source, flow2.destination))
+                new SwapFlowPayload(flow1.id, flow1Src, flow1Dst),
+                new SwapFlowPayload(flow2.id, flow2Src, flow2Dst))
+
         def flow1Updated = northbound.getFlow(flow1.id)
         def flow2Updated = northbound.getFlow(flow2.id)
 
         then: "Endpoints are successfully swapped"
-        response.firstFlow.source == flow2.source
-        response.firstFlow.destination == flow1.destination
+        response.firstFlow.source == flow1Src
+        response.firstFlow.destination == flow1Dst
+        response.secondFlow.source == flow2Src
+        response.secondFlow.destination == flow2Dst
 
-        response.secondFlow.source == flow1.source
-        response.secondFlow.destination == flow2.destination
+        flow1Updated.source == flow1Src
+        flow1Updated.destination == flow1Dst
+        flow2Updated.source == flow2Src
+        flow2Updated.destination == flow2Dst
 
-        flow1Updated.source == flow2.source
-        flow1Updated.destination == flow1.destination
-
-        flow2Updated.source == flow1.source
-        flow2Updated.destination == flow2.destination
-
-        when: "Try to swap destination endpoints for flows"
-        response = northbound.swapFlowEndpoint(
-                new SwapFlowPayload(flow1.id, flow2.source, flow2.destination),
-                new SwapFlowPayload(flow2.id, flow1.source, flow1.destination))
-        flow1Updated = northbound.getFlow(flow1.id)
-        flow2Updated = northbound.getFlow(flow2.id)
-
-        then: "Endpoints are successfully swapped"
-        response.firstFlow.source == flow2.source
-        response.firstFlow.destination == flow2.destination
-
-        response.secondFlow.source == flow1.source
-        response.secondFlow.destination == flow1.destination
-
-        flow1Updated.source == flow2.source
-        flow1Updated.destination == flow2.destination
-
-        flow2Updated.source == flow1.source
-        flow2Updated.destination == flow1.destination
+        (flow1Path == PathHelper.convert(northbound.getFlowPath(flow1.id))) == pathsAreEqual
+        (flow2Path == PathHelper.convert(northbound.getFlowPath(flow2.id))) == pathsAreEqual
 
         and: "Delete flows"
         [flow1, flow2].each { flowHelper.deleteFlow(it.id) }
+
+        where:
+        switchPair << [getTopologyHelper().getNotNeighboringSwitchPair()] * 4
+        flow1 << [getFlowHelper().randomFlow(switchPair)] * 4
+        flow2 << [getFlowHelper().randomFlow(switchPair, false, [flow1])] * 4
+        [flow1Src, flow1Dst, flow2Src, flow2Dst] << [
+                [flow2.source, flow1.destination, flow1.source, flow2.destination],
+                [flow1.source, flow2.destination, flow2.source, flow1.destination],
+                [flow2.destination, flow1.destination, flow2.source, flow1.source],
+                [flow1.source, flow2.source, flow1.destination, flow2.destination]
+        ]
+        pathsAreEqual << [true, true, false, false]
     }
 }
