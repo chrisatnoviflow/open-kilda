f8913cd1b63c2c273d2a18c16736ffdd4b82af03
Yaroslav Lobankov <lobankovyv@gmail.com>
2019-05-23 14:37:13 +0300

    Add one more test

--- services/src/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/flows/SwapEndpointSpec.groovy
+++ services/src/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/flows/SwapEndpointSpec.groovy
@@ -2,11 +2,63 @@ package org.openkilda.functionaltests.spec.flows
 
 import org.openkilda.functionaltests.BaseSpecification
 import org.openkilda.functionaltests.helpers.PathHelper
+import org.openkilda.functionaltests.helpers.model.SwitchPair
+import org.openkilda.messaging.payload.flow.FlowEndpointPayload
 import org.openkilda.messaging.payload.flow.SwapFlowPayload
 
 import spock.lang.Unroll
 
 class SwapEndpointSpec extends BaseSpecification {
+
+    @Unroll
+    def "Able to swap VLAN endpoints for flows with the same source and destination switches"() {
+        given: "Two flows with the same source and destination switches"
+        flowHelper.addFlow(flow1)
+        flowHelper.addFlow(flow2)
+
+        when: "Try to swap VLAN endpoints for flows"
+        def flow1Path = PathHelper.convert(northbound.getFlowPath(flow1.id))
+        def flow2Path = PathHelper.convert(northbound.getFlowPath(flow2.id))
+
+        def response = northbound.swapFlowEndpoint(
+                new SwapFlowPayload(flow1.id, flow1Src, flow1Dst),
+                new SwapFlowPayload(flow2.id, flow2Src, flow2Dst))
+
+        def flow1Updated = northbound.getFlow(flow1.id)
+        def flow2Updated = northbound.getFlow(flow2.id)
+
+        then: "VLAN endpoints are successfully swapped"
+        response.firstFlow.source == flow1Src
+        response.firstFlow.destination == flow1Dst
+        response.secondFlow.source == flow2Src
+        response.secondFlow.destination == flow2Dst
+
+        flow1Updated.source == flow1Src
+        flow1Updated.destination == flow1Dst
+        flow2Updated.source == flow2Src
+        flow2Updated.destination == flow2Dst
+
+        flow1Path == PathHelper.convert(northbound.getFlowPath(flow1.id))
+        flow2Path == PathHelper.convert(northbound.getFlowPath(flow2.id))
+
+        and: "Delete flows"
+        [flow1, flow2].each { flowHelper.deleteFlow(it.id) }
+
+        where:
+        switchPair << [getTopologyHelper().getNotNeighboringSwitchPair()] * 4
+        flow1 << [getFlowHelper().randomFlow(switchPair)] * 4
+        flow2 << [getFlowHelper().randomFlow(switchPair, false, [flow1])] * 4
+        [flow1Src, flow1Dst, flow2Src, flow2Dst] << [
+                [changePropertyValue(flow1.source, "vlanId", flow2.source.vlanId), flow1.destination,
+                 changePropertyValue(flow2.source, "vlanId", flow1.source.vlanId), flow2.destination],
+                [flow1.source, changePropertyValue(flow1.destination, "vlanId", flow2.destination.vlanId),
+                 flow2.source, changePropertyValue(flow2.destination, "vlanId", flow1.destination.vlanId)],
+                [changePropertyValue(flow1.source, "vlanId", flow2.destination.vlanId), flow1.destination,
+                 flow2.source, changePropertyValue(flow2.destination, "vlanId", flow1.source.vlanId)],
+                [flow1.source, changePropertyValue(flow1.destination, "vlanId", flow2.source.vlanId),
+                 changePropertyValue(flow2.source, "vlanId", flow1.destination.vlanId), flow2.destination]
+        ]
+    }
 
     @Unroll
     def "Able to swap endpoints for flows with the same source and destination switches"() {
@@ -154,9 +206,6 @@ class SwapEndpointSpec extends BaseSpecification {
     @Unroll
     def "Able to swap endpoints for flows with different source and destination switches"() {
         given: "Two flows with different source and destination switches"
-        println(flow1SwitchPair)
-        println(flow2SwitchPair)
-
         flowHelper.addFlow(flow1)
         flowHelper.addFlow(flow2)
 
@@ -201,14 +250,19 @@ class SwapEndpointSpec extends BaseSpecification {
         ]
     }
 
-    def getHalfDifferentNotNeighboringSwitchPair(switchPairToDiffer, equalEndpoint, differentEndpoint) {
+    FlowEndpointPayload changePropertyValue(FlowEndpointPayload object, String propertyName, newValue) {
+        object.tap { it."$propertyName" = newValue }
+    }
+
+    SwitchPair getHalfDifferentNotNeighboringSwitchPair(SwitchPair switchPairToDiffer, String equalEndpoint,
+                                                        String differentEndpoint) {
         getTopologyHelper().getAllNotNeighboringSwitchPairs().find {
             it."$equalEndpoint" == switchPairToDiffer."$equalEndpoint" &&
                     it."$differentEndpoint" != switchPairToDiffer."$differentEndpoint"
         }
     }
 
-    def getDifferentNotNeighboringSwitchPair(switchPairToDiffer) {
+    SwitchPair getDifferentNotNeighboringSwitchPair(SwitchPair switchPairToDiffer) {
         getTopologyHelper().getAllNotNeighboringSwitchPairs().find {
             it.src != switchPairToDiffer.src && it.dst != switchPairToDiffer.dst
         }
